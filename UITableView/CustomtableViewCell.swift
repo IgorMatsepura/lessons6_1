b60405dd31bdd8497c94381de5c77c71d574ff23
Igor <igor.matsepura@gmail.com>
2018-03-12 10:13:16 +0200

    lessons6_1 ver 1.0

---
+++ UITableView/CustomtableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomtableViewCell.swift
+//  UITableView
+//
+//  Created by IgorMac on 3/9/18.
+//  Copyright © 2018 IgorMac. All rights reserved.
+//
+
+import UIKit
+
+class CustomtableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
